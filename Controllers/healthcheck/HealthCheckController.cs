e967bed1d871d6c6ba9002bd7e4125f26a4ecb96
shweta ji <shweta.76544@gmail.com>
2026-02-12 22:46:34 +0530

    Initial backend setup with health check API

---
+++ Controllers/healthcheck/HealthCheckController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace backened_for_intern.Controllers.healthcheck
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HealthCheckController : ControllerBase
+    {
+
+        [HttpGet]
+        public IActionResult HealthCheck()
+        {
+            return Ok("Ok sabb thik hai serverver chal raha hai--> http://localhost:5052");
+        }
+
+    }
+}
